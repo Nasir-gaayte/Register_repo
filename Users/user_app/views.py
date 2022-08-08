eafe77e0e580d93ce252e934e9b10ab4f884a4fb
nasir-gaayte <slowboyz12@gmail.com>
2022-08-08 23:20:03 +0300

    last of the first day

--- Users/user_app/views.py
+++ Users/user_app/views.py
@@ -1,7 +1,8 @@
+import cmath
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
-from django.contrib.auth import login, logout,authenticate
+from django.contrib.auth import login, logout,authenticate, user_logged_in
 from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
@@ -18,7 +19,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=True)
+            user=form.save(commit=True)
             login(request,user)
             messages.success(request,"you are in ")
             return redirect('home')
@@ -39,7 +40,7 @@ def login_req(request):
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
-                login(request, user)
+                login(request,user)
                 messages.info(request,f"You don't have a {username}.?")
                 return redirect('home')
             else:
